a55b60393f7211d60aea187242a899621f527c04
Oscar Guerrero Lopez <oscar.computer.guy@gmail.com>
2021-05-08 19:10:42 +0200

    FIX: wrong import

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,23 +1,27 @@
+import { SanitiseBody } from "auth-ogl";
 import { NextFunction, Request, Response } from "express";
 
 import HttpException from "../exceptions/HttpException";
-import { WinstonLoggerWrapper } from "../utils/logger";
+import { Logger } from "../utils/logger";
 
 export const errorMiddleware = (
   error: HttpException,
-  _request: Request,
+  request: Request,
   response: Response,
   _next: NextFunction,
-) => {
+): void => {
   const status = error.status || 500;
   const message = error.message || "Something went wrong";
 
-  WinstonLoggerWrapper({
-    level: "error",
-    message,
-    status,
+  Logger.error(message, {
     identifier: "ErrorMiddleware",
-    stack: JSON.stringify(error),
+    error: {
+      message: error.message || "no error message",
+      stack: error.stack || "no stack",
+    },
+    code: request.code,
+    body: SanitiseBody(request.body),
+    headers: request.headers,
   });
 
   response.status(status).send({
